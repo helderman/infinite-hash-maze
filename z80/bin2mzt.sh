b079d2297f020f7fcee527f4f4e26dc395d1dd21
Ruud Helderman <r.helderman@hccnet.nl>
2019-12-09 22:46:01 +0100

    Correct file size in MZ-700 tape header

---
+++ z80/bin2mzt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# bin2mzt.sh - prepends a 128-byte tape header to a Z80 object file
+# (assumes start and execute address are 4000h)
+# Example: bin2mzt.sh foo.bin > foo.mzt
+
+NAME=${1^^}
+NAME=${NAME%.BIN}
+
+echo -ne '\01'
+echo -n "$NAME"
+echo -ne '\x0D'
+printf '%*s' $((16 - ${#NAME}))
+printf '%04x' $(wc -c < "$1") | xxd -p -r | rev
+echo -ne '\0@\0@\0\0\0\0\0\0\0\0'
+echo -ne '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+echo -ne '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+echo -ne '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+cat "$1"
