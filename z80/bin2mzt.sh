b532f2d21a05a0dadd364d265656c080bb1eb5fe
Ruud Helderman <r.helderman@hccnet.nl>
2020-03-21 23:55:54 +0100

    Auto-detect start address

--- z80/bin2mzt.sh
+++ z80/bin2mzt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # bin2mzt.sh - prepends a 128-byte tape header to a Z80 object file
-# Usage: bin2mzt.sh <input file> <load address> <execute address>
-# Example: bin2mzt.sh foo.bin 0x4000 0x4000 > foo.mzt
+# Usage: bin2mzt.sh <object file> <label file>
+# Example: bin2mzt.sh foo.bin foo.lbl > foo.mzt
 
 function padding {
 	dd bs=$1 conv=sync,ucase status=none
@@ -12,11 +12,13 @@ function output_word {
 }
 
 NAME="$(basename "$1" .bin)"
+LOAD_ADDRESS="$(grep -oiP '^START:\s*equ\s+\$\K\w+' "$2")"
+EXEC_ADDRESS="$(grep -oiP '^START:\s*equ\s+\$\K\w+' "$2")"
 
 echo -n $'\1'"${NAME::16}"$'\r' | padding 18
 output_word $(wc -c < "$1")
-output_word "$2"
-output_word "$3"
+output_word 0x${LOAD_ADDRESS:-1200}
+output_word 0x${EXEC_ADDRESS:-1200}
 echo -ne '\0' | padding 104
 
 cat "$1"
